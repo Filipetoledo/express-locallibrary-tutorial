44562c20d078d97418e03dcbaea847ded48f2655
SalisDev <alissongselau@gmail.com>
2023-07-25 10:57:21 -0300

    Users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-const express = require("express");
-const router = express.Router();
+
+
+var express = require("express");
+var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
+});
+
+router.get("/cool", function (req, res, next) {
+  res.send("you so cool");
+});
+
+router.get("/ifc", function (req, res, next) {
+  res.send("Bem vindo ao IFC");
+});
+
+router.get("/cool/beach", function (req, res, next) {
+  res.send("Adoro Praia");
 });
 
 module.exports = router;
